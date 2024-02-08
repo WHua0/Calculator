c495161a1dd373a3f2aa1d19afa7ed125b2ee65e
whua0 <wh44@njit.edu>
2024-02-08 17:47:44 -0500

    updated definitions

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -18,28 +18,26 @@ def divide(a,b):
 
 def check_number(num):
     '''Checks if input is a number'''
-    while True:
+    try:
+        int(num)
+        return float(num)
+    except ValueError:
         try:
-            int(num)
-            return float(num)
+            float(num)
+            return float (num)
         except ValueError:
-            try:
-                float(num)
-                return float (num)
-            except ValueError:
-                print(num, "is not valid number!")
-                num = input("Enter a Number: ")
+            print(num, "is not valid number!")
+            return "error"
 
 def check_operation(op):
     '''Checks if input is a math operation'''
-    while True:
-        if op in ("addition", "add", "+"):
-            return "addition"
-        if op in ("subtraction", "subtract", "-"):
-            return "subtraction"
-        if op in ("multiplication", "multiply", "*"):
-            return "multiplication"
-        if op in ("division", "divide", "/"):
-            return "division"
-        print(op, "is not valid math operation!")
-        op = input("Enter a Operation: ").lower()
+    if op in ("addition", "add", "+"):
+        return "addition"
+    if op in ("subtraction", "subtract", "-"):
+        return "subtraction"
+    if op in ("multiplication", "multiply", "*"):
+        return "multiplication"
+    if op in ("division", "divide", "/"):
+        return "division"
+    print(op, "is not valid math operation!")
+    return "error"
