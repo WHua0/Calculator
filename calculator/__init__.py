1b07e9468a66491364c3891c17dd7b6d369670fb
whua0 <wh44@njit.edu>
2024-02-08 11:39:15 -0500

    Added Basic Calculator Functions

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,4 +1,17 @@
-'''Calculator Definitions'''
+'''Definitions'''
 
-# def add(a,b):
-    # return a + b
+def add(a,b):
+    '''Add a and b'''
+    return a + b
+
+def subtract(a,b):
+    '''Subtract a and b'''
+    return a - b
+
+def multiply(a,b):
+    '''Multiple a and b'''
+    return a * b
+
+def divide(a,b):
+    '''Divide a by b'''
+    return a / b
