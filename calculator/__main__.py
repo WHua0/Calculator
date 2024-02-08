e2facd687150c876a0053e1516e374a12eb1c7e2
whua0 <wh44@njit.edu>
2024-02-08 12:57:49 -0500

    Added Main Program

---
+++ calculator/__main__.py
@@ -0,0 +1,15 @@
+'''Calculator Program'''
+from calculator import add, subtract, multiply, divide
+
+def print_intro():
+    '''Introduction'''
+    print("Welcome User!")
+    print("This is a Basic Calculator Program.")
+    print("Created by Wilber Hua")
+
+def main():
+    '''Main Program'''
+    print_intro()
+
+main()
+input("\nPress <Enter> to quit")
