5664ec3e59179c19905297eba9a57b7d856798b7
whua0 <wh44@njit.edu>
2024-02-09 15:05:07 -0500

    Added staticmethod to operations

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,17 +1,20 @@
 '''Function: Operations'''
-
+@staticmethod
 def add(a,b):
     '''Add a and b'''
     return a + b
 
+@staticmethod
 def subtract(a,b):
     '''Subtract a and b'''
     return a - b
 
+@staticmethod
 def multiply(a,b):
     '''Multiple a and b'''
     return a * b
 
+@staticmethod
 def divide(a,b):
     '''Divide a by b'''
     if b == 0:
