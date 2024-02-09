6b71a0f7a92bba655a1159388f6bf69f0752b401
whua0 <wh44@njit.edu>
2024-02-09 13:26:37 -0500

    Moved operations to operations.py

---
+++ calculator/operations.py
@@ -0,0 +1,17 @@
+'''Definitions'''
+
+def add(a,b):
+    '''Add a and b'''
+    return a + b
+
+def subtract(a,b):
+    '''Subtract a and b'''
+    return a - b
+
+def multiply(a,b):
+    '''Multiple a and b'''
+    return a * b
+
+def divide(a,b):
+    '''Divide a by b'''
+    return a / b
