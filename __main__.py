92b888009b6f049a87e0d56d7644077057caf10e
whua0 <wh44@njit.edu>
2024-02-08 19:04:56 -0500

    Updated main

--- __main__.py
+++ __main__.py
@@ -13,34 +13,47 @@ def main():
     '''Main Program'''
     print_intro()
 
-    # Input Numbers and Math Operations
+    # Input first number
     number1 = input("Enter a Number: ")
-    number1 = check_number(number1)
 
-    operation = (input("Enter a Operation: ")).lower()
+    # If Input returns error, try again
+    while check_number(number1) == "error":
+        number1 = input("Enter a Number: ")
+    number1 = float(number1)
+
+    # Input math operation
+    operation = input("Enter a Operation: ")
+
+    # If Input returns error, try again
+    while check_operation(operation) == "error":
+        operation = input("Enter a Operation: ")
     operation = check_operation(operation)
 
-    number2 = input("Enter a 2nd Number: ")
-    number2 = check_number(number2)
+    # Input second number
+    number2 = input("Enter a Number: ")
+
+    # If Input returns error, try again
+    while check_number(number2) == "error":
+        number2 = input("Enter a Number: ")
+    number2 = float(number2)
 
     # Calculate
-    if operation == "addition":
-        add(number1,number2)
+    result = ""
 
-    elif operation == "subtraction":
-        subtract(number1,number2)
+    if operation == "+":
+        result = add(number1,number2)
 
-    elif operation == "multiplication":
-        multiply(number1,number2)
+    if operation == "-":
+        result = subtract(number1,number2)
 
-    elif operation == "division":
-        if number2 == 0:
-            print("Cannot Divide By 0!")
-        else:
-            divide(number1,number2)
+    if operation == "*":
+        result = multiply(number1,number2)
 
-    else:
-        pass
+    if operation == "/":
+        result = divide(number1,number2)
+
+    # Print Result
+    print(number1, operation, number2, "=", result)
 
 main()
 
