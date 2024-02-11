a79984bab2cf631b265a80f3bcd04f8fc77c0244
whua0 <wh44@njit.edu>
2024-02-11 13:11:04 -0500

    testing append()

--- calculator/history.py
+++ calculator/history.py
@@ -1,20 +1,20 @@
 '''Manages Calculations History'''
 import dataclasses
-from typing import List
-from calculator import Calculation
 
 @dataclasses.dataclass
 class History:
     '''Class History'''
+    history = []
 
-    history: List[Calculation] = []
-
-    # '''Add a new calculation to the history'''
+    @classmethod
+    def add_calculation(cls, calculation):
+        '''Add a new calculation to the history'''
+        cls.history.append(calculation)
 
     # '''Retrieve the entire calculation history'''
 
-    # '''Clear the calculation history'''
+    # Optional - '''Clear the calculation history'''
 
     # Optional - '''Retrieve the lastest calculation, but returns None if there is no history'''
 
-    # Optional - '''Find and reutnr a list of calculations by operation  name'''
+    # Optional - '''Find and return a list of calculations by operation  name'''
---
+++ tests/test_history.py
@@ -0,0 +1,9 @@
+'''History Test'''
+from calculator.history import History
+from calculator.operations import add
+
+def test_add_calculation():
+    '''test that add calculation function works'''
+    calc = (2, 2, add)
+    History.add_calculation(calc)
+    assert len(history) == 1
