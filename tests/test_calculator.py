030909cf3b15177ed66fcc42133210126cebed8d
whua0 <wh44@njit.edu>
2024-02-15 14:40:58 -0500

    Edited to pass Pyests after adding pytest_generate_tests(metafunc)

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,6 +4,7 @@
 # pylint: disable = expression-not-assigned
 
 '''Calculator Test'''
+from decimal import Decimal
 import pytest
 from calculator import Calculator
 from calculator.operations import Operation
@@ -15,14 +16,14 @@ def test_operations(a, b, operation, expected):
 def test_division_by_zero():
     '''Tests that divide by zero exception'''
     with pytest.raises(ValueError, match = 'Cannot divide by zero!'):
-        Calculator.execute(2, 0, Operation.divide), 'Divide by Zero exception failed'
+        Calculator.execute(Decimal('2'), Decimal('0'), Operation.divide), 'Divide by Zero exception failed'
 
 @pytest.fixture
 def setup_history():
     '''Clears history and adds sample calculations for tests'''
     Calculator.clear_history()
-    Calculator.execute(2, 2, Operation.add)
-    Calculator.execute(2, 2, Operation.subtract)
+    Calculator.execute(Decimal('2'), Decimal('2'), Operation.add)
+    Calculator.execute(Decimal('2'), Decimal('2'), Operation.subtract)
 
 def test_history(setup_history):
     '''Tests that history show previous, show history, and clear history functions work'''
