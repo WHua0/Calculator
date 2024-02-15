a76d0d3b0f1241fb3460830ae370e6f26a561ce2
whua0 <wh44@njit.edu>
2024-02-15 12:37:33 -0500

    Updated Tests

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,8 +1,14 @@
+# pylint: disable = unused-argument
 # pylint: disable = expression-not-assigned
+# pylint: disable = line-too-long
 
 '''Operation Test'''
 import pytest
+from calculator.calculation import Calculation
 from calculator.operations import Operation
+from tests import conftest
+
+
 def test_addition():
     '''Tests that add function works'''
     assert Operation.add(2, 2) == 4, 'Add operation failed'
@@ -23,3 +29,9 @@ def test_division_by_zero():
     '''Tests that divide by zero exception'''
     with pytest.raises(ValueError, match = 'Cannot divide by zero!'):
         Operation.divide(2, 0), 'Divide by Zero exception failed'
+
+@pytest.mark.parametrize("a, b, operation_name, operation, expected", conftest.generate_test_data(10))
+def test_operations(a, b, operation_name, operation, expected):
+    '''Tests various operations'''
+    calculation = Calculation(a, b, operation)
+    assert calculation.compute() == expected, f'{operation.__name__} operation failed'
