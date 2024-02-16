98b98ad225c617730b80e052c123f7a5f1d23577
whua0 <wh44@njit.edu>
2024-02-16 10:13:26 -0500

    Added comments

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 import sys
 from decimal import Decimal, InvalidOperation
 from calculator import Calculator
-from calculator import operations
 
 def calculate_and_print(a, b, operation_name):
     '''Calculate and Print Results'''
@@ -19,15 +18,27 @@ def calculate_and_print(a, b, operation_name):
     }
 
     try:
+        # Tries to convert a and b into decimal objects
         a_decimal, b_decimal = map(Decimal, [a, b])
+        # Retrieves operation function from the operation Name in operation Mappings
         result = operation_mappings.get(operation_name)
+
+        # If operation function is found => calculates and prints
         if result:
             print(f'The result of {a} {operation_name} {b} is equal to {result(a_decimal, b_decimal)}')
+
+        # If operation function is not found => unknown operation
         else:
             print(f'Unknown operation: {operation_name}')
+
+    # If a and b cannot be converted to decimal objects => invalid number
     except InvalidOperation:
         print(f'Invalid number input: {a} or {b} is not a valid number.')
+
+    # If result is ZeroDvisionError from b = 0 => Error: Division by zero.
     except ZeroDivisionError:
         print('Error: Division by zero.')
+
+    # Catch-All for Unexpected Errors
     except Exception as e:
         print(f'An error occured: {e}')
