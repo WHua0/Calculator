d04df06e72be8552b27fd5c5fb671a1885ff6069
whua0 <wh44@njit.edu>
2024-02-16 09:37:52 -0500

    Added Calculator.add(a, b) etc

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -2,6 +2,7 @@
 from decimal import Decimal
 from calculator.calculation import Calculation
 from calculator.history import History
+from calculator.operations import Operation
 
 class Calculator:
     '''Class Calculator'''
@@ -14,6 +15,26 @@ class Calculator:
         log = History.create_log(a, b, operation)
         History.add_calculation(log)
         return calculation.compute()
+
+    @staticmethod
+    def add(a: Decimal, b: Decimal):
+        '''Executes add'''
+        return Calculator.execute(a, b, Operation.add)
+
+    @staticmethod
+    def subtract(a: Decimal, b: Decimal):
+        '''Executes subtract'''
+        return Calculator.execute(a, b, Operation.add)
+
+    @staticmethod
+    def multiply(a: Decimal, b: Decimal):
+        '''Executes multiply'''
+        return Calculator.execute(a, b, Operation.add)
+
+    @staticmethod
+    def divide(a: Decimal, b: Decimal):
+        '''Executes divide'''
+        return Calculator.execute(a, b, Operation.add)
 
     @staticmethod
     def show_history():
