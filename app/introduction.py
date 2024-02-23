643d6775c52ee2ac218036629f9f4a91b840fc91
whua0 <wh44@njit.edu>
2024-02-23 15:12:17 -0500

    Added Introduction

---
+++ app/introduction.py
@@ -0,0 +1,8 @@
+'''Introduction'''
+
+def introduction():
+    '''Prints an Introduction'''
+    print('Calculator App Initiated.')
+    print('Please type a command.')
+    print('Type "menu" for details.')
+    print('Type "exit" to exit.')
