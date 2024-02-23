4230d7652c0c6e96e365f90261597522400084e3
whua0 <wh44@njit.edu>
2024-02-23 16:56:39 -0500

    Passed Exit Test

---
+++ tests/test_exit.py
@@ -0,0 +1,25 @@
+# pylint: disable = redefined-outer-name
+
+'''Exit Test'''
+import sys
+import pytest
+from app.exit import ExitCommand
+
+@pytest.fixture
+def exit_command():
+    '''Define Fixture'''
+    return ExitCommand()
+
+def test_execute(exit_command, monkeypatch):
+    '''Tests Exit Command'''
+    exit_msg = None
+
+    def mock_exit(msg):
+        nonlocal exit_msg
+        exit_msg = msg
+
+    monkeypatch.setattr(sys, "exit", mock_exit)
+
+    exit_command.execute()
+
+    assert exit_msg == 'Exiting Calculator App ...'
