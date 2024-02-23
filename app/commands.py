7970e1ec716fba66a26aaa3b172ca16d50d858b1
whua0 <wh44@njit.edu>
2024-02-23 15:54:33 -0500

    Updated CommandHandler

---
+++ app/commands.py
@@ -0,0 +1,32 @@
+# pylint: disable=too-few-public-methods
+
+'''Command Manager'''
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    '''Class Command'''
+
+    @abstractmethod
+    def execute(self):
+        '''Any Subclass of Command must implement its own execute method'''
+        # Passthrough
+
+class CommandHandler:
+    '''Class CommandHandler'''
+
+    def __init__(self):
+        '''Constructor for a Dictionary'''
+        self.commands = {}
+
+    def register_command(self, command_name: str, command: Command):
+        '''Registers the Command, Str : Abstract Class Command'''
+        self.commands[command_name] = command
+
+    def execute_command(self, command_name: str):
+        '''Tries to Execute the Command'''
+        try:
+            self.commands[command_name].execute()
+
+        # If Fails to Execute => KeyError: Invalid Command
+        except KeyError:
+            print(f"Invalid Command: {command_name}")
