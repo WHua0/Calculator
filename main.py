4515c018ae194a15514df6186ea0107677712685
whua0 <wh44@njit.edu>
2024-02-23 15:16:33 -0500

    Updated comments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 '''Calculator Main Application'''
 from app import App
 
+# Code will only be activated when the script is directly run, not when imported
 if __name__ == '__main__':
     App().start()
