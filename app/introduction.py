b6fa0f18d5cefb2a0ff869c71753a8a02e790aa0
whua0 <wh44@njit.edu>
2024-02-23 15:21:48 -0500

    Added spacing

--- app/introduction.py
+++ app/introduction.py
@@ -2,7 +2,10 @@
 
 def introduction():
     '''Prints an Introduction'''
+    print()
     print('Calculator App Initiated.')
+    print()
     print('Please type a command.')
     print('Type "menu" for details.')
     print('Type "exit" to exit.')
+    print()
