b7ac9d27bd443d165e397c02b5727d82ff461803
whua0 <wh44@njit.edu>
2024-02-24 17:24:51 -0500

    Updated Comments

--- app/__init__.py
+++ app/__init__.py
@@ -9,31 +9,45 @@ class App:
 
     def __init__(self):
         '''Constructor'''
-        self.command_handler = CommandManager()
+        self.command_manager = CommandManager()
 
     def load_plugins(self):
         ''' Dynamically load all plugins in the plugins directory'''
+
+        # Indicates where the plugins are expected to be found
         plugins_package = 'app.plugins'
+
+        # Iterates through the modules in plugins_pacakge directory
         for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
-            if is_pkg:  # Ensure it's a package
+
+            # If module is a package, imports the module
+            if is_pkg:
                 plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+
+                # Iterates over all items defined in that module
                 for item_name in dir(plugin_module):
                     item = getattr(plugin_module, item_name)
+
                     try:
-                        if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
-                            self.command_handler.register_command(plugin_name, item())
+                        # Checks if item is a subclass of Command
+                        if issubclass(item, (Command)):
+                            # If yes, instantiates the class and registers it with command manager
+                            self.command_manager.register_command(plugin_name, item())
+
                     except TypeError:
-                        continue  # If item is not a class or unrelated class, just ignore
+                        # If not, ignores
+                        continue
 
     def start(self):
         '''Starts the App'''
+
         # Prints Introduction
         introduction()
 
-        # Register commands
+        # Registers Commands
         self.load_plugins()
 
         # REPL: Read, Evaluate, Print, Loop
-        # Tries to Execute the Input Command Name, then Loops
+        # Tries to Read, Evaluate, Execute the Input Command Name, then Loops
         while True:
-            self.command_handler.execute_command(input(">>> ").strip())
+            self.command_manager.execute_command(input(">>> ").strip())
