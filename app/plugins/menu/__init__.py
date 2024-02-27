bf23a1924b71542ee05bdc84c49cdf11702579f4
whua0 <wh44@njit.edu>
2024-02-27 16:17:44 -0500

    Updated Menu Commant to show all plugin names

--- app/plugins/menu/__init__.py
+++ app/plugins/menu/__init__.py
@@ -5,6 +5,13 @@ from app.commandmanager import Command
 
 class MenuCommand(Command):
     '''Class MenuCommand'''
+
+    def __init__(self, command_manager):
+        '''Constructor'''
+        self.command_manager = command_manager
+
     def execute(self):
-        # Prints Command List"
-        print('Commands: add, subtract, multiply, divide.')
+        '''Executes method to show all plugin/command names'''
+        print('Commands:')
+        for plugin_name in self.command_manager.commands.keys():
+            print(plugin_name)
