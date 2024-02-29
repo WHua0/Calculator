75bc6f9a4670f476471309187c11da3e4a19cfe2
whua0 <wh44@njit.edu>
2024-02-29 09:21:46 -0500

    Refactored

--- script.py
+++ script.py
@@ -4,41 +4,7 @@
 '''Calculator Script'''
 
 import sys
-from decimal import Decimal, InvalidOperation
-from calculator import Calculator
-
-def calculate_and_print(a, b, operation_name):
-    '''Calculate and Print Results'''
-
-    # Defines operation mappings, string => function
-    operation_mappings = {
-        'add': Calculator.add,
-        'subtract': Calculator.subtract,
-        'multiply': Calculator.multiply,
-        'divide': Calculator.divide
-    }
-
-    try:
-        # Tries to convert a and b into decimal objects
-        a_decimal, b_decimal = map(Decimal, [a, b])
-        # Retrieves operation function from the operation name in operation mappings
-        operation_function = operation_mappings.get(operation_name)
-
-        # If operation function is found => calculates and prints
-        if operation_function:
-            print(f'The result of {a} {operation_name} {b} is equal to {operation_function(a_decimal, b_decimal)}.')
-
-        # If operation function is not found => unknown operation
-        else:
-            print(f'Unknown operation: {operation_name}.')
-
-    # If a and b cannot be converted to decimal objects => invalid number
-    except InvalidOperation:
-        print(f'Invalid number input: {a} or {b} is not a valid number.')
-
-    # Catch-All for Unexpected Errors
-    except Exception as e:
-        print(f'An error occurred: {e}.')
+from calculator.calculateprint import calculate_and_print
 
 def cli_command():
     '''Entry point for Python Script'''
